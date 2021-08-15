ec2dca2bb07c55eafdb87a2fa805138e2f8ea771
yeonkyung <younkyong94@gmail.com>
2021-08-15 22:58:33 +0900

    zuul-service

---
+++ zuul-service/src/main/java/com/example/zuulservice/filter/ZuulLoggingFilter.java
@@ -0,0 +1,42 @@
+package com.example.zuulservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    // Logger logger = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+    @Override
+    public Object run() throws ZuulException {
+
+        log.info("-------------- print log --------------");
+
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        log.info("requestURI : " + request.getRequestURI());
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+}
